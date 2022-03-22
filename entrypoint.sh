cc87f81686f415099273333ae9df35be1aa6dc10
Clement Fradet Normand <Aarbel@users.noreply.github.com>
2022-03-23 00:17:08 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 
-testim --token $1 --project $2 --grid $3 -r testim-report.xml ${4:+--suite $4} ${5:+--base-url $5} ${6:+--result-label $6} ${7:+--timeout $7} ${8:+--branch $8} ${9:+--parallel $9} ${10:+--retries ${10}} ${11:+--label ${11}}
+testim --token $1 --project $2 --grid $3 -r testim-report.xml ${4:+--suite $4} ${5:+--base-url $5} ${6:+--result-label $6} ${7:+--timeout $7} ${8:+--branch $8} ${9:+--parallel $9} ${10:+--retries ${10}} ${11:+--label ${11}} ${12:+--override-execution-name ${12}}
