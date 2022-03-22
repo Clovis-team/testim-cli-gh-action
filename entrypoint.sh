9413354765b9da1dd572d11bcf8293057552685c
Clement Fradet Normand <Aarbel@users.noreply.github.com>
2022-03-22 21:40:46 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo ">> START TESTIM CLI"
+echo $@
 testim --token $1 --project $2 --grid $3 -r testim-report.xml ${4:+--suite $4} ${5:+--base-url $5} ${6:+--result-label $6} ${7:+--timeout $7} ${8:+--branch $8} ${9:+--parallel $9} ${10:+--retries $10} ${11:+--label $11}
